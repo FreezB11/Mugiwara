c953f9faf9da29fff8cf9d1f73c3a944d47d28f7
FreezB11 <yashrajrao4@gmail.com>
2026-04-20 20:23:04 +0530

    update with isr and idt

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,10 @@
 #include "gdt.h"
+#include "idt.h"
+#include "isr.h"
 
 void kernel_main(void) {
     gdt_init();
+    isr_install();
     volatile unsigned short *vga = (volatile unsigned short *)0xB8000;
     vga[0] = 0x0F00 | 'K';
 
