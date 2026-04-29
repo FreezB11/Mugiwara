4803974d2622ddb8c5d6e69c6aa327f27864dae1
FreezB11 <yashrajrao4@gmail.com>
2026-04-30 01:43:10 +0530

    ports to help me here and there

---
+++ kernel/ports.c
@@ -0,0 +1,11 @@
+#include "ports.h"
+
+void outb(u16 port, u8 value) {
+    asm volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
+u8 inb(u16 port) {
+    u8 ret;
+    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
---
+++ kernel/ports.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "util.h"
+
+void outb(u16 port, u8 value);
+u8 inb(u16 port);
